5ceab71dad17f514d5ae96f430feba0e49e66b89
Mai Thiên Phú <phumaithien22@gmail.com>
2021-12-10 23:28:58 +0700

    implement base of puzzle y2021-d8

---
+++ src/main/java/vn/pmt/year2021/day8/Input218.java
@@ -0,0 +1,10 @@
+package vn.pmt.year2021.day8;
+
+import vn.pmt.common.PuzzleInput;
+
+/**
+ * @author Mai Thiên Phú
+ * @since 10/12/2021
+ */
+public class Input218 implements PuzzleInput {
+}
---
+++ src/main/java/vn/pmt/year2021/day8/Solver218.java
@@ -0,0 +1,46 @@
+package vn.pmt.year2021.day8;
+
+import static vn.pmt.year2021.day8.Solver218.*;
+
+import java.util.List;
+
+import vn.pmt.common.puzzle.Puzzle;
+import vn.pmt.common.puzzle.AbstractPuzzleSolver;
+
+/**
+ * @author Mai Thiên Phú
+ * @since 08/12/2021
+ */
+@Puzzle(year = YEAR, day = DAY)
+public class Solver218 extends AbstractPuzzleSolver<Input218, Result218> {
+    public static final int YEAR = 2021;
+    public static final int DAY = 8;
+
+    public Solver218() {
+    }
+
+    @Override
+    protected int year() {
+        return YEAR;
+    }
+
+    @Override
+    protected int day() {
+        return DAY;
+    }
+
+    @Override
+    protected Input218 parseInput(List<String> rawInput) {
+        return null;
+    }
+
+    @Override
+    protected boolean testPart1(Result218 result) {
+        return false;
+    }
+
+    @Override
+    protected void displayResult(Result218 result) {
+
+    }
+}
