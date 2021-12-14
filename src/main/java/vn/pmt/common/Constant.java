f30cb0cb66fd938b584fcb2b2d552ba8e69c8674
Mai Thiên Phú <phumaithien22@gmail.com>
2021-12-14 22:03:58 +0700

    year 2021 day 14 implementation

--- src/main/java/vn/pmt/common/Constant.java
+++ src/main/java/vn/pmt/common/Constant.java
@@ -8,4 +8,5 @@ public class Constant {
     public static final String COLON_DELIMITER = ":";
     public static final String SPACE_DELIMITER = " ";
     public static final String HYPHEN_DELIMITER = "-";
+    public static final String ARROW_SPACE_DELIMITER = " -> ";
 }
