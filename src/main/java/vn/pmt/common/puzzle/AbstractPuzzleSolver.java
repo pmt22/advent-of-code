c202efeb3f0d355fcc7fb9899df8a68db843bec7
Mai Thiên Phú <phumaithien22@gmail.com>
2021-12-26 00:15:39 +0700

    refactor - add stopwatch

--- src/main/java/vn/pmt/common/puzzle/AbstractPuzzleSolver.java
+++ src/main/java/vn/pmt/common/puzzle/AbstractPuzzleSolver.java
@@ -1,6 +1,8 @@
 package vn.pmt.common.puzzle;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.lang3.time.StopWatch;
 
 import vn.pmt.common.Importer;
 import vn.pmt.common.PuzzleInput;
@@ -14,6 +16,10 @@ public abstract class AbstractPuzzleSolver<I extends PuzzleInput, R extends Puzz
     private final Importer importer = new Importer();
     private List<String> rawTestInput;
     private List<String> rawActualInput;
+    private I parsedTestInput;
+    private I parsedActualInput;
+
+    protected StopWatch stopWatch = StopWatch.create();
 
     protected abstract int year();
     protected abstract int day();
@@ -24,8 +30,24 @@ public abstract class AbstractPuzzleSolver<I extends PuzzleInput, R extends Puzz
     @Override
     public void solve() throws Exception {
         importResources();
+
+        stopWatch.start();
+        parsedTestInput = parseInput(rawTestInput);
+        parsedActualInput = parseInput(rawActualInput);
+        stopWatch.stop();
+        System.out.println("Parsing input execution time: " + stopWatch.getTime(TimeUnit.MILLISECONDS) + " ms");
+
+        stopWatch.reset();
+        stopWatch.start();
         part1();
+        stopWatch.stop();
+        System.out.println("Part 1 execution time: " + stopWatch.getTime(TimeUnit.MILLISECONDS) + " ms");
+
+        stopWatch.reset();
+        stopWatch.start();
         part2();
+        stopWatch.stop();
+        System.out.println("Part 2 execution time: " + stopWatch.getTime(TimeUnit.MILLISECONDS) + " ms");
     }
 
     private void importResources() throws Exception {
@@ -35,13 +57,11 @@ public abstract class AbstractPuzzleSolver<I extends PuzzleInput, R extends Puzz
 
     private void part1() {
         System.out.println("----------Part 1----------");
-        I parsedTestInput = parseInput(rawTestInput);
         R testResult = proposeSolutionPart1(parsedTestInput);
         System.out.print("Test: ");
         displayResult(testResult);
         if (testPart1(testResult)) {
-            I parsedInput = parseInput(rawActualInput);
-            R result = proposeSolutionPart1(parsedInput);
+            R result = proposeSolutionPart1(parsedActualInput);
             System.out.print("Actual: ");
             displayResult(result);
         }
@@ -49,13 +69,11 @@ public abstract class AbstractPuzzleSolver<I extends PuzzleInput, R extends Puzz
 
     private void part2() {
         System.out.println("----------Part 2----------");
-        I parsedTestInput = parseInput(rawTestInput);
         R testResult = proposeSolutionPart2(parsedTestInput);
         System.out.print("Test: ");
         displayResult(testResult);
         if (testPart2(testResult)) {
-            I parsedInput = parseInput(rawActualInput);
-            R result = proposeSolutionPart2(parsedInput);
+            R result = proposeSolutionPart2(parsedActualInput);
             System.out.print("Actual: ");
             displayResult(result);
         }
